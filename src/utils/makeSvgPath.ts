ae374a9458457af6f8b3788af0737148f2a05557
huangjin <519730485@qq.com>
2021-01-26 21:41:02 +0800

    update BezierCurve

--- src/utils/makeSvgPath.ts
+++ src/utils/makeSvgPath.ts
@@ -1,53 +1,52 @@
-/**
- * Rounds coordinates
- */
-// @ts-nocheck
 
 import { ICoordinateType } from '../types'
 
 const roundPoint = (point: ICoordinateType): ICoordinateType => [Math.floor(point[0]), Math.floor(point[1])]
 
-/**
- * Given a source point and an output point produces the SVG path between them
- */
-const makeSvgPath = (startPoint?: ICoordinateType, endPoint?: ICoordinateType) => {
-  if (!startPoint || !endPoint) return ''
-  const roundedStart = roundPoint(startPoint)
-  const roundedEnd = roundPoint(endPoint)
-
-  const start = `${roundedStart[0]}, ${roundedStart[1]}`
-  const end = `${roundedEnd[0]}, ${roundedEnd[1]}`
-
-
-  // return `M ${start}, ${end}`
-  return makeBezierCurve({x: roundedStart[0], y: roundedStart[1]}, {x: roundedEnd[0], y: roundedEnd[1]})
-}
-
-export default makeSvgPath
-
-const makeBezierCurve = (from: any, to: any) => {
-  if (to.x >= from.x + 40 || Math.abs(to.y - from.y) <= 40) {
+// 贝塞尔曲线算法
+const makeBezierCurve = (from: ICoordinateType, to: ICoordinateType) => {
+  if (to[0] >= from[0] + 40 || Math.abs(to[1] - from[1]) <= 40) {
     return getCubicBezierPath(from, to)
   } else {
     return getAdvancedCubicBezierPath(from, to)
   }
 }
 
-const getPointsString = (points: any) => {
-  return points.map((item) => `${item.x},${item.y}`).join(' ')
+const getPointsString = (points: ICoordinateType[]) => {
+  return points.map((item) => `${item[0]},${item[1]}`).join(' ')
 }
 
-const getCubicBezierPath = (from: any, to: any) => {
-  const controlPointForStart = {x: (to.x + from.x) / 2, y: from.y}
-  const controlPointForEnd = {x: (to.x + from.x) / 2, y: to.y}
+// 正向 贝塞尔曲线
+const getCubicBezierPath = (from: ICoordinateType, to: ICoordinateType) => {
+  const controlPointForStart: ICoordinateType = [(to[0] + from[0]) / 2, from[1]]
+  const controlPointForEnd: ICoordinateType = [(to[0] + from[0]) / 2, to[1]]
 
-  return `M ${from.x},${from.y} C ${getPointsString([controlPointForStart, controlPointForEnd])} ${to.x},${to.y}`
+  return `M ${from[0]},${from[1]} C ${getPointsString([controlPointForStart, controlPointForEnd])} ${to[0]},${to[1]}`
 }
 
+// 反向 贝塞尔曲线
+const getAdvancedCubicBezierPath = (from: ICoordinateType, to: ICoordinateType) => {
+  const midX = (from[0] + to[0]) / 2
+  const midY = (from[1] + to[1]) / 2
 
-const getAdvancedCubicBezierPath = (from: any, to: any) => {
-  const midX = (from.x + to.x) / 2
-  const midY = (from.y + to.y) / 2
-
-  return `M${from.x},${from.y} ${from.x + 20},${from.y} C ${from.x + 100},${from.y} ${from.x + 130},${(midY + from.y) / 2}  ${midX},${midY} C ${midX},${midY} ${to.x - 130},${(midY + to.y) / 2} ${to.x - 2},${to.y} M ${to.x - 2},${to.y} ${to.x},${to.y}`
+  return `M${from[0]},${from[1]} ${from[0] + 20},${from[1]} C ${from[0] + 100},${from[1]} ${from[0] + 130},${(midY + from[1]) / 2}  ${midX},${midY} C ${midX},${midY} ${to[0] - 130},${(midY + to[1]) / 2} ${to[0] - 2},${to[1]} M ${to[0] - 2},${to[1]} ${to[0]},${to[1]}`
 }
+
+/**
+ * 生成连点之间的贝塞尔曲线
+ */
+const makeSvgPath = (startPoint?: ICoordinateType, endPoint?: ICoordinateType) => {
+  if (!startPoint || !endPoint) return ''
+  const roundedStart = roundPoint(startPoint)
+  const roundedEnd = roundPoint(endPoint)
+
+  // const start = `${roundedStart[0]}, ${roundedStart[1]}`
+  // const end = `${roundedEnd[0]}, ${roundedEnd[1]}`
+
+  // return `M ${start}, ${end}`
+  return makeBezierCurve(roundedStart, roundedEnd)
+}
+
+export default makeSvgPath
+
+
