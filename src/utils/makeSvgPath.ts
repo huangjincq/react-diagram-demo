1bddedad703ff6979fa77c1b50d06cf19cf1c29d
huangjin <519730485@qq.com>
2021-01-26 21:31:41 +0800

    add BezierCurve

--- src/utils/makeSvgPath.ts
+++ src/utils/makeSvgPath.ts
@@ -1,10 +1,11 @@
 /**
  * Rounds coordinates
  */
-import {ICoordinateType} from "../types";
+// @ts-nocheck
 
-const roundPoint = (point: ICoordinateType) => [Math.floor(point[0]), Math.floor(point[1])]
+import { ICoordinateType } from '../types'
 
+const roundPoint = (point: ICoordinateType): ICoordinateType => [Math.floor(point[0]), Math.floor(point[1])]
 
 /**
  * Given a source point and an output point produces the SVG path between them
@@ -18,9 +19,35 @@ const makeSvgPath = (startPoint?: ICoordinateType, endPoint?: ICoordinateType) =
   const end = `${roundedEnd[0]}, ${roundedEnd[1]}`
 
 
-  const ctrl = `${roundedEnd[0]}, ${roundedStart[1]}`
-
-  return `M ${start} Q ${ctrl}, ${end}`
+  // return `M ${start}, ${end}`
+  return makeBezierCurve({x: roundedStart[0], y: roundedStart[1]}, {x: roundedEnd[0], y: roundedEnd[1]})
 }
 
 export default makeSvgPath
+
+const makeBezierCurve = (from: any, to: any) => {
+  if (to.x >= from.x + 40 || Math.abs(to.y - from.y) <= 40) {
+    return getCubicBezierPath(from, to)
+  } else {
+    return getAdvancedCubicBezierPath(from, to)
+  }
+}
+
+const getPointsString = (points: any) => {
+  return points.map((item) => `${item.x},${item.y}`).join(' ')
+}
+
+const getCubicBezierPath = (from: any, to: any) => {
+  const controlPointForStart = {x: (to.x + from.x) / 2, y: from.y}
+  const controlPointForEnd = {x: (to.x + from.x) / 2, y: to.y}
+
+  return `M ${from.x},${from.y} C ${getPointsString([controlPointForStart, controlPointForEnd])} ${to.x},${to.y}`
+}
+
+
+const getAdvancedCubicBezierPath = (from: any, to: any) => {
+  const midX = (from.x + to.x) / 2
+  const midY = (from.y + to.y) / 2
+
+  return `M${from.x},${from.y} ${from.x + 20},${from.y} C ${from.x + 100},${from.y} ${from.x + 130},${(midY + from.y) / 2}  ${midX},${midY} C ${midX},${midY} ${to.x - 130},${(midY + to.y) / 2} ${to.x - 2},${to.y} M ${to.x - 2},${to.y} ${to.x},${to.y}`
+}
