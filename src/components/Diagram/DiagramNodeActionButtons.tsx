435f223924b384208aa2b567d19c04c2deae0bb1
huangjin <519730485@qq.com>
2021-02-28 10:35:53 +0800

    update delete copy style

--- src/components/Diagram/DiagramNodeActionButtons.tsx
+++ src/components/Diagram/DiagramNodeActionButtons.tsx
@@ -1,5 +1,7 @@
 import React, { useCallback } from 'react'
 import { Button } from 'antd'
+import { DeleteOutlined, CopyOutlined } from '@ant-design/icons'
+
 
 // import { DeleteOutlined } from '@ant-design/icons'
 
@@ -22,9 +24,9 @@ export const DiagramNodeActionButtons: React.FC<DiagramNodeActionButtonsProps> =
   }, [id, onNodeCopy])
 
   return (
-    <div>
-      <Button onClick={handleNodeCopy}>复制</Button>
-      <Button onClick={handleNodeDelete}>删除</Button>
+    <div className='diagram-node-action'>
+      <Button onClick={handleNodeCopy} icon={<CopyOutlined/>}/>
+      <Button onClick={handleNodeDelete} icon={<DeleteOutlined/>}/>
     </div>
   )
 }
