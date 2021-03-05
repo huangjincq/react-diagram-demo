af8dbb8018440a7fad24800230b14606db26c420
huangjin <519730485@qq.com>
2021-03-05 18:32:52 +0800

    add some icon

--- src/components/Toolbar/ShortcutsPanel.tsx
+++ src/components/Toolbar/ShortcutsPanel.tsx
@@ -1,7 +1,9 @@
-import React, { useCallback, useMemo } from 'react'
-import { Button, Popover } from 'antd'
+import React, { useCallback } from 'react'
 
 import './style.scss'
+import IconClick from '../../static/icon-click.png'
+import IconMove from '../../static/icon-move.png'
+import IconWheel from '../../static/icon-wheel.png'
 
 export interface ShortcutsPanelProps {}
 
@@ -13,14 +15,14 @@ const ALT_OR_OPT = isMac ? 'OPT' : 'Alt'
 const hotkeyList = [
   { text: '撤销', hotKeys: [{ key: CTRL_OR_CMD }, { key: 'Z' }] },
   { text: '重做', hotKeys: [{ key: CTRL_OR_CMD }, { key: 'Shift' }, { key: 'Z' }] },
-  { text: '缩放', hotKeys: [{ icon: 'a' }] },
+  { text: '缩放', hotKeys: [{ icon: IconWheel }] },
   { text: '复制', hotKeys: [{ key: CTRL_OR_CMD }, { key: 'C' }] },
   { text: '粘贴', hotKeys: [{ key: CTRL_OR_CMD }, { key: 'V' }] },
   { text: '全选', hotKeys: [{ key: CTRL_OR_CMD }, { key: 'A' }] },
   { text: '自动排列', hotKeys: [{ key: CTRL_OR_CMD }, { key: 'Shift' }, { key: 'A' }] },
   { text: '删除', hotKeys: [{ key: 'Del' }] },
-  { text: '多选', hotKeys: [{ key: 'Shift' }, { icon: '2' }] },
-  { text: '移动画布', hotKeys: [{ key: 'Space' }, { icon: '2' }] },
+  { text: '多选', hotKeys: [{ key: 'Shift' }, { icon: IconMove }] },
+  { text: '移动画布', hotKeys: [{ key: 'Space' }, { icon: IconMove }] },
 ]
 
 export const ShortcutsPanel: React.FC<ShortcutsPanelProps> = React.memo(({}) => {
@@ -29,6 +31,7 @@ export const ShortcutsPanel: React.FC<ShortcutsPanelProps> = React.memo(({}) => 
       {hotkeyList.map((item, index) => (
         <div key={index} className="scale-item">
           {item.text}
+          <img src={IconWheel} alt="" />
         </div>
       ))}
     </div>
