d9d52ba4939c29d3a0cd209b05a9864be49e6c91
huangjin <519730485@qq.com>
2021-04-26 15:33:14 +0800

    更新自动排列算法

--- src/components/NodeTypes/NodeTypeInput.tsx
+++ src/components/NodeTypes/NodeTypeInput.tsx
@@ -1,29 +1,38 @@
 import React from 'react'
-import { Input } from 'antd'
-
+import { Input, Collapse } from 'antd'
 import './style.scss'
 import { INodeItemProps } from '../../types'
 import { NodeTypeHeader } from './NodeTypeHeader'
 import { nodesConfig } from './config'
+const { Panel } = Collapse
 
-export interface NodeTypeInputProps extends INodeItemProps<any> {
-}
-
+const text = `A dog is a type`
+export interface NodeTypeInputProps extends INodeItemProps<any> {}
 
 export const NodeTypeInput: React.FC<NodeTypeInputProps> = (props) => {
-
-  const {value, onChange} = props
+  const { value, onChange } = props
   const handleInputChange = (e: any) => {
     onChange({
       ...value,
-      inputValue: e.target.value
+      inputValue: e.target.value,
     })
   }
 
   return (
     <>
-      <NodeTypeHeader icon={nodesConfig.nodeTypeInput.icon} label={nodesConfig.nodeTypeInput.label}/>
-      <Input value={value.inputValue} onChange={handleInputChange} placeholder="Basic usage"/>
+      <NodeTypeHeader icon={nodesConfig.nodeTypeInput.icon} label={nodesConfig.nodeTypeInput.label} />
+      <Input value={value.inputValue} onChange={handleInputChange} placeholder="Basic usage" />
+      <Collapse defaultActiveKey={['1']}>
+        <Panel header="This is panel header 1" key="1">
+          <p>{text}</p>
+        </Panel>
+        <Panel header="This is panel header 2" key="2">
+          <p>{text}</p>
+        </Panel>
+        <Panel header="This is panel header 3" key="3">
+          <p>{text}</p>
+        </Panel>
+      </Collapse>
     </>
   )
 }
